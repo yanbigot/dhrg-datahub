25670eabdf9f6a0a14cc2053844b72de2f3aa640
yanbigot <yanbigot@gmail.com>
2018-05-27 23:59:48 +0200

    U00.bis component

--- src/main/scala/com/yb/backend/AppBackEnd.scala
+++ src/main/scala/com/yb/backend/AppBackEnd.scala
@@ -2,6 +2,33 @@ package com.yb.backend
 
 object AppBackEnd {
 
+  /**
+    * Source
+    * {"block": "1", "value": [{"x":"A"}, {"x":"B"}]}
+    * {"block": "2", "value": [{"y":"C","h":"D"}, {"y":"E","h":"F"}]}
+    *
+    * Explode on "value"
+    * {"block": "1", "value": {"x":"A"}}
+    * {"block": "1", "value": {"x":"B"}}
+    * {"block": "2", "value": {"y":"C","h":"D"}}
+    * {"block": "2", "value": {"y":"E","h":"F"}}
+    *
+    * Save to BlockView
+    *
+    * Join
+    * {"x":"A", "y":"C", "h":"D"}
+    * {"x":"B", "y":"E", "h":"F"}
+    *
+    * Filter and rename
+    * {"name":"A", "age":"C", "gender":"D"}
+    * {"name":"B", "age":"E", "gender":"F"}
+    *
+    * Functional controls (not yet implemented)
+    *
+    * Save to DraftView
+    *
+  */
+
   def main( args: Array[String] ): Unit = {
     val rawView   = Raw.load
     val blockView = Block.build(rawView)
