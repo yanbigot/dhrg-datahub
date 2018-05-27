b9d3458857fe161bf0ae363f3e3dc930547a27f0
yanbigot <yanbigot@gmail.com>
2018-05-27 15:52:56 +0200

    training, source to pre draft

--- src/main/scala/com/yb/RawBlockSource.scala
+++ src/main/scala/com/yb/RawBlockSource.scala
@@ -1,57 +1,50 @@
 package com.yb
 
+import java.util.Date
 
-
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import com.yb.generator.TrainingGenerator
+import net.liftweb.json.JObject
+import org.apache.spark.sql.functions._
+//import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
 
 object RawBlockSource {
+  var count = 0
   val trainingFile = "D:\\NIFI_WORKSPACE\\dhrg-datahub\\src\\main\\resources\\trainingCamel.json"
   val trainingBaseFile = "D:\\NIFI_WORKSPACE\\dhrg-datahub\\src\\main\\resources\\trainingBaseCamel.json"
-
-  def sparkSession = {
-    val spark = SparkSession
-      .builder()
-      .appName("Simple Spark Application")
-      .master("local[*]")
-      .getOrCreate()
-    spark
-  }
-
-  case class Conf(endPoint: String, schema: StructType)
-
-  /**
-    * Simulates a loaded input file configuration
-    * @return
-    */
-  def buildConf(): Seq[Conf] ={
-    Seq(
-      Conf("training_base",
-        fieldNames2StructType(
-          Seq("endPoint","objectId", "learningName"))),
-      Conf("training_session",
-        fieldNames2StructType(
-          Seq("endPoint","sessionObjectId", "sessionName", "trainFk"))),
-      Conf("training_price",
-        fieldNames2StructType(
-          Seq("endPoint","priceObjectId", "priceAmount", "tSessFk")))
+  val trainingNestedFile = "D:\\NIFI_WORKSPACE\\dhrg-datahub\\trainings-merged.json"
+//  val trainingNestedFile = "D:\\NIFI_WORKSPACE\\dhrg-datahub\\src\\main\\resources\\training-nested.json"
+  val joinConfPath = "D:\\NIFI_WORKSPACE\\dhrg-datahub\\src\\main\\resources\\join.json"
+  val learningDraftConf = "D:\\NIFI_WORKSPACE\\dhrg-datahub\\src\\main\\resources\\learning-draft.json"
+  val date = new Date().getTime
+  val trainingGenericSchema =
+    StructType(
+      Seq(
+        StructField("source", StringType, false),
+        StructField("autre", StringType, true),
+        StructField("value",
+          DataTypes.createArrayType(
+            DataTypes.createMapType(StringType, StringType, true)
+            , true))
+      )
     )
-  }
+  implicit val formats = net.liftweb.json.DefaultFormats
 
-  /**
-    * Generates a dynamic schema
-    * @param fields
-    * @return
-    */
-  def fieldNames2StructType(fields: Seq[String]): StructType ={
-    val schema = ArrayBuffer[StructField]()
-    schema.appendAll(
-      fields.map(x => new StructField(x, StringType)).toList
-    )
-    StructType(schema)
-  }
+  case class BlockConf( endPoint: String, schema: StructType )
+
+  case class JoinConf( draft: String, rightTable: String, rightKey: String, leftTable: String, leftKey: String, composition: Array[JoinConf] )
+
+  case class BlockLocation( endpointName: String, path: String )
+
+  case class NamedDataFrame( endpointName: String, df: DataFrame )
+
+  case class Draft( name: String, df: DataFrame )
+
+  case class LearningDraft()
 
   /**
     * Read a json file where each line has a different schema
@@ -59,45 +52,223 @@ object RawBlockSource {
     * This way we can use spark native json support in a columnar way
     * Then in order to generate a super set of blocks allowing to create a Draft in
     * a 1 Source (n blocks) is enough to generate a Draft entity
+    *
     * @param args
     */
   def main( args: Array[String] ): Unit = {
-    val spark: SparkSession = sparkSession
-    import spark.implicits._
-    // read line by line
-    val file: Dataset[String] = spark.read.textFile(trainingFile)
-    //iterate on file as dataframe : each block is a dedicated dataframe
-    var dfs:Map[String,DataFrame] = Map()
-    for(c  <- buildConf){
-      dfs += (c.endPoint -> createBlockDf(c, spark, file))
-    }
-    dfs.foreach(x => x._2.printSchema())
-    dfs.foreach(x => x._2.show(false))
+//    TrainingGenerator.process(10)
+    TrainingGenerator.process(1000000)
+    ss.sparkContext.setLogLevel("WARN")
+    process
+  }
 
-    //defined set of join condition on a source view perspective
-    val joinCondition = Seq("training_base#objectId","training_session#trainFk" )
-    val split_A = joinCondition(0).split("#")
-    val split_B = joinCondition(1).split("#")
-    val df_A_Conf = (split_A(0), split_A(1))
-    val df_B_Conf = (split_B(0), split_B(1))
-    //simulate iteration on df list
-    val dfa = dfs.get(df_A_Conf._1).get
-    val dfb = dfs.get(df_B_Conf._1).get
-    //simulate iterate on join conditions => generates a draft
-    val draft = dfa.join(dfb, dfa.col(df_A_Conf._2) === dfb.col(df_B_Conf._2), "left")
-    draft.show(false)
+  def ss = {
+    SparkSession
+      .builder()
+      .appName("Simple Spark Application")
+      .master("local[*]")
+      .config("spark.sql.warehouse.dir","file:///C:/Experiment/spark-2.0.0-bin-without-hadoop/spark-warehouse")
+      .getOrCreate()
   }
 
   /**
-    * Creates dataframe by filter based on endpoint name
-    * @param endpoint
-    * @param spark
-    * @param file
+    * Simulates a loaded input file configuration
+    *
     * @return
     */
-  def createBlockDf(endpoint: Conf, spark: SparkSession, file: Dataset[String]): DataFrame = {
-    import spark.implicits._
-    spark.read.schema(endpoint.schema).json(file.filter(l => l.contains(endpoint.endPoint)))
+  def loadBlockConfiguration( ): Seq[BlockConf] = {
+    Seq(
+      BlockConf("training",
+        fieldNames2StructType(
+          Seq("endPoint", "trainingObjectId", "learningName"))),
+      BlockConf("session",
+        fieldNames2StructType(
+          Seq("endPoint", "sessionObjectId", "sessionName", "trainingFk"))),
+      BlockConf("price",
+        fieldNames2StructType(
+          Seq("endPoint", "priceObjectId", "priceAmount", "sessionFk")))
+    )
+  }
+
+  def loadJoinConf( ): Array[JoinConf] = {
+    val json = Source.fromFile(joinConfPath)
+    net.liftweb.json.parse(json.mkString).extract[Array[JoinConf]]
+  }
+  def loadDraftMappingConf( path: String ): StructType = {
+    val json = Source.fromFile(learningDraftConf)
+    val conf = net.liftweb.json.parse(json.mkString)
+      .extract[JObject]
+      .values.keySet
+      .toSeq
+    fieldNames2StructType(conf)
+  }
+
+  /**
+    * Generates a dynamic schema
+    *
+    * @param fields
+    * @return
+    */
+  def fieldNames2StructType( fields: Seq[String] ): StructType = {
+    val schema = ArrayBuffer[StructField]()
+    schema.appendAll(
+      fields.map(x => new StructField(x, StringType)).toList
+    )
+    StructType(schema)
+  }
+
+  def process( ): Unit = {
+    /**
+      * Load the file where each line as a different json schema as a map of prop/value, eg:
+      * {"block": "1", "value": [{"x":"A"}, {"x":"B"}]}
+      * {"block": "2", "value": [{"y":"C","h":"D"}, {"y":"E","h":"F"}]}
+      */
+    val fileDf = loadWithTrainingGenericSchema
+    fileDf.show()
+    /**
+      * Explode on value property, eg:
+      * {"block": "1", "value": {"x":"A"}}
+      * {"block": "1", "value": {"x":"B"}}
+      * {"block": "2", "value": {"y":"C","h":"D"}}
+      * {"block": "2", "value": {"y":"E","h":"F"}}
+      */
+    val fileDfExploded = explodeValueArrayWithTrainingGenericSchema(fileDf)
+    /**
+      * Save each different "block" to a different parquet file
+      */
+    val blocksLocation: Seq[BlockLocation] = saveAsParquet(fileDfExploded)
+    blocksLocation.foreach(println)
+    val blockDfs: Map[String, DataFrame] = blocksLocation.map(
+      bl => (bl.endpointName -> ss.read.parquet(bl.path))
+    ).toMap
+    blockDfs.foreach(bdf => bdf._2.show(false))
+    /**
+      * Load joiner configuration, one root element per Draft to generate
+      */
+    val joins = loadJoinConf
+    /**
+      * A draft needs several blocks materialised as dataframes to be joined to be generated
+      * Each Draft will result in a DataFrame with its name
+      */
+    val draftDfs: Map[String, DataFrame] = joins.map(j => (j.draft -> createDraft(blockDfs, j))).toMap
+    draftDfs.foreach(ddf => {
+      val learningDraftSchema = loadDraftMappingConf(learningDraftConf) //oui ca mock dur
+//      learningDraftSchema.printTreeString()
+      println(ddf._1)
+      ddf._2.show(false)
+      ddf._2.printSchema()
+      val preDraft = ss.createDataFrame(ddf._2.rdd, learningDraftSchema)
+      preDraft.printSchema()
+        preDraft
+          .write.parquet("spark-warehouse/drafts/" + ddf._1 + "-" + date + ".parquet" )
+    })
+
+
+
+  }
+
+  def createDraft( blockDfs: Map[String, DataFrame], joinConf: JoinConf ): DataFrame = {
+    println("Draft root: " + joinConf.leftTable)
+    val left: DataFrame = blockDfs.get(joinConf.leftTable).get
+    recurseJoin(joinConf, left, blockDfs)
+  }
+
+  def recurseJoin( jc: JoinConf, left: DataFrame, blockDfs: Map[String, DataFrame] ): DataFrame = {
+    count = count + 1
+    var joined = {
+      if (jc.rightTable == null)
+        left
+      else
+        joinWithConf(jc, left, blockDfs)
+    }
+    if (!jc.composition.isEmpty){
+      for (joinConf <- jc.composition) {
+        println("--- recurse")
+        joined.printSchema()
+        joined = recurseJoin(joinConf, joined, blockDfs)
+      }
+    }
+    joined
+  }
+
+  def joinWithConf( jc: JoinConf, left: DataFrame, blockDfs: Map[String, DataFrame] ): DataFrame = {
+    //process current join
+    println("*********************"+count+"****************************")
+    println("   --> " + count + ": " + jc.rightTable + "." + jc.rightKey + " = " + jc.leftTable + "." + jc.leftKey)
+//    left.printSchema()
+    println(" -- LEFT -- ")
+    left.show(false)
+    val right: DataFrame = blockDfs.get(jc.rightTable).get
+//    right.printSchema()
+    println(" -- RIGHT -- ")
+    right.show(false)
+    println("*********************"+count+"****************************")
+    left.join(right, left.col(jc.leftKey) === right.col(jc.rightKey), "left_outer")
+  }
+
+  def saveAsParquet( nestedExploded: DataFrame ): Seq[BlockLocation] = {
+    loadBlockConfiguration
+      .map(conf => (conf.endPoint, toParquetFile(conf, nestedExploded)))
+      .map(t => BlockLocation(t._1, getParquetFileNameFromLocalDir(t._2)))
+  }
+
+  def testParquetFromLocal( fileNames: Seq[String] ): Unit = {
+    fileNames
+      .map(name => ss.read.parquet(name))
+      .foreach(df => {
+        df.show(false)
+        df.printSchema
+      }
+      )
+  }
+
+  def loadWithTrainingGenericSchema( ): DataFrame = {
+    ss.read.schema(trainingGenericSchema).json(trainingNestedFile)
+  }
+
+  def explodeValueArrayWithTrainingGenericSchema( nestedFile: DataFrame ): DataFrame = {
+    import nestedFile.sparkSession.implicits._
+    nestedFile
+      .select($"source", explode($"value").as("value"))
+      .select(to_json($"value").as("line"))
+  }
+
+  def getParquetFileNameFromLocalDir( dir: String ): String = {
+    getListOfFiles(dir)
+      .filter(f => f.getAbsolutePath.endsWith("parquet"))
+      .mkString
+  }
+  def getListOfFiles( dir: String ): List[java.io.File] = {
+    val d = new java.io.File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else {
+      List[java.io.File]()
+    }
+  }
+
+  def toParquetFile( endpoint: BlockConf, file: DataFrame ): String = {
+    import file.sparkSession.implicits._
+    val fileName = "spark-warehouse/" + endpoint.endPoint + "-" + date
+    //    println("----------- full !!!!!!!!")
+    //    file.show(false)
+    //    file.printSchema()
+    //
+    //    println("----------- filter on full !!!!!!!!")
+    //
+    //    file.filter($"line".contains(endpoint.endPoint)).show(false)
+    //    endpoint.schema.printTreeString()
+    val df = ss
+      .read
+      .schema(endpoint.schema)
+      .json(file.filter($"line".contains(endpoint.endPoint)).as[String])
+    //    println("----------- filtered !!!!!!!!")
+    //    df.show(false)
+    df
+      .coalesce(1)
+      .write
+      .parquet(fileName)
+    fileName
   }
 
 }
---
+++ src/main/scala/com/yb/generator/TrainingGenerator.scala
@@ -0,0 +1,137 @@
+package com.yb.generator
+
+import java.io.{File, PrintWriter}
+
+import net.liftweb.json.DefaultFormats
+import net.liftweb.json.Serialization.write
+
+import scala.io.Source
+
+object TrainingGenerator {
+  implicit val formats = DefaultFormats
+  var MAX = 10
+  val MOD_SESSION = 2
+  val MOD_PRICE = 3
+  case class Training( endPoint: String = "training", trainingObjectId: String, learningName: String )
+  case class Session( endPoint: String = "session", sessionObjectId: String, sessionName: String, trainingFk: String )
+  case class Price( endPoint: String = "price", priceObjectId: String, priceAmount: String, sessionFk: String )
+
+  def main( args: Array[String] ): Unit = {
+    process(MAX)
+  }
+
+  def process(max: Int): Unit ={
+    MAX = max
+    genStream()
+    merge()
+  }
+
+  def lastTurn(max: Int, mod: Int): Int ={
+    (max) - (max % mod)
+  }
+
+  def genStream( ) {
+    val writerTrainings = new PrintWriter(new File("trainings-dummy.json"))
+    val writerSessions = new PrintWriter(new File("sessions-dummy.json"))
+    val writerPrices = new PrintWriter(new File("prices-dummy.json"))
+
+    val template =
+      """
+        |{"source": "#", "autre": "other", "value": [
+        |""".stripMargin
+    writerTrainings.write(template.replace("#", "training"))
+    writerSessions.write(template.replace("#", "session"))
+    writerPrices.write(template.replace("#", "price"))
+
+    val lastTraining = lastTurn(MAX, 1)
+    val lastSession = lastTurn(MAX, MOD_SESSION)
+    val lastPrice = lastTurn(MAX, MOD_PRICE)
+//    println("MAX: "+MAX+" >> lastTraining: " + lastTraining)
+//    println("MAX: "+MAX+" >> lastSession : " + lastSession)
+//    println("MAX: "+MAX+" >> lastPrice   : " + lastPrice)
+
+    for (i <- 1 until MAX + 1) {
+
+      var sepTraining: String = {
+        if(i >= lastTraining)
+          ""
+        else
+          ","
+      }
+      var sepSession: String = {
+        if(i >= lastSession)
+          ""
+        else
+          ","
+      }
+      var sepPrice: String = {
+        if(i >= lastPrice)
+          ""
+        else
+          ","
+      }
+//      println("turn: "+ i + ", max "+ MAX + " sepPrice: " + sepPrice + " - sepSession: "+sepSession)
+//      println("turn: "+ i + ", max "+ MAX + " lastSession: " + lastSession + " - lastPrice: "+lastPrice)
+//
+//      println(" - write Training")
+      writerTrainings.write(
+        write(Training(trainingObjectId = "" + i, learningName = "learningName_" + i)))
+
+      writerTrainings.write(sepTraining)
+
+      if (i % MOD_SESSION == 0) {
+//        println(" - write Session")
+        writerSessions.write(
+          write(Session(sessionObjectId = "" + i * 3, trainingFk = "" + i, sessionName = "sessionName_" + i)))
+
+        writerSessions.write(",")
+
+        writerSessions.write(
+          write(Session(sessionObjectId = "" + i * 5, trainingFk = "" + i, sessionName = "sessionName_" + i)))
+
+        writerSessions.write(sepSession)
+      }
+      if (i % MOD_PRICE == 0) {
+//        println(" - write Price ")
+        writerPrices.write(
+          write(Price(priceObjectId = "" + i * 3, priceAmount = "" + i * 4, sessionFk = "" + i)))
+
+        writerPrices.write(",")
+
+        writerPrices.write(
+          write(Price(priceObjectId = "" + i * 5, priceAmount = "" + i * 8, sessionFk = "" + i)))
+
+        writerPrices.write(",")
+
+        writerPrices.write(
+          write(Price(priceObjectId = "" + i * 10, priceAmount = "" + i * 40, sessionFk = "" + i)))
+
+        writerPrices.write(sepPrice)
+      }
+    }
+    writerTrainings.write("]}")
+    writerPrices.write("]}")
+    writerSessions.write("]}")
+
+    writerTrainings.close()
+    writerPrices.close()
+    writerSessions.close()
+  }
+
+  def merge( ): Unit = {
+    val merger = new PrintWriter(new File("trainings-merged.json"))
+//    Source.fromFile("trainings-dummy.json").foreach(c => merger.write(c))
+//    Source.fromFile("sessions-dummy.json").foreach(c => merger.write(c))
+//    Source.fromFile("prices-dummy.json").foreach(c => merger.write(c))
+    partWrite(merger, "trainings-dummy.json")
+    partWrite(merger, "sessions-dummy.json")
+    partWrite(merger, "prices-dummy.json")
+    merger.close
+  }
+
+  def partWrite(w: PrintWriter, path: String): Unit ={
+    Source.fromFile(path).getLines().foreach(l => w.write(l))
+    w.flush()
+    w.write("\r\n")
+  }
+}
